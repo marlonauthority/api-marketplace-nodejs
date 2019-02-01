6cd0bf6451f1b7adfdd54499bcac305267225751
marlondck <15050029+marlondck@users.noreply.github.com>
2019-02-01 17:21:30 -0300

    mongoose

---
+++ src/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  uri:
+    'mongodb://marlon:13071992@marketplacerocket-shard-00-00-cnxvv.mongodb.net:27017,marketplacerocket-shard-00-01-cnxvv.mongodb.net:27017,marketplacerocket-shard-00-02-cnxvv.mongodb.net:27017/test?ssl=true&replicaSet=marketplaceRocket-shard-0&authSource=admin&retryWrites=true'
+}
