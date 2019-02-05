56b48548d133c27e4f40b3b4bf379948da92a931
marlondck <15050029+marlondck@users.noreply.github.com>
2019-02-04 22:39:44 -0300

    paginate

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -3,7 +3,15 @@ const Ad = require('../models/Ad')
 class AdController {
   async index (req, res) {
     // Busca todos ads
-    const ads = await Ad.find()
+    const ads = await Ad.paginate(
+      {},
+      {
+        page: req.query.page || 1,
+        limit: 20,
+        populate: ['author'],
+        sort: '-createdAt'
+      }
+    )
     return res.json(ads)
   }
   async show (req, res) {
