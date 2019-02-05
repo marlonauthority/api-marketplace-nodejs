bd79f1d930e1015a4a491986584fd9761d7af9b1
marlondck <15050029+marlondck@users.noreply.github.com>
2019-02-05 16:17:43 -0300

    validacao

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const validate = require('express-validation')
 const routes = express.Router()
 const authMiddleware = require('./app/middlewares/auth')
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 /** * User ***/
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 /** * Autenticacao */
 routes.use(authMiddleware)
@@ -13,11 +23,15 @@ routes.use(authMiddleware)
 /** * Adsense **/
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /** * Compras */
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
