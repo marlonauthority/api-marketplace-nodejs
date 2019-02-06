94cab978641b603ad934699802e65dcb3c88aaed
marlondck <15050029+marlondck@users.noreply.github.com>
2019-02-05 21:38:01 -0300

    ENV

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-  uri:
-    'mongodb://marlon:13071992@marketplacerocket-shard-00-00-cnxvv.mongodb.net:27017,marketplacerocket-shard-00-01-cnxvv.mongodb.net:27017,marketplacerocket-shard-00-02-cnxvv.mongodb.net:27017/test?ssl=true&replicaSet=marketplaceRocket-shard-0&authSource=admin&retryWrites=true'
+  uri: process.env.DB_URL
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://a6f420a692004ba2910737c08777e8ba@sentry.io/1387724'
+  dsn: process.env.SENTRY_DSN
 }
